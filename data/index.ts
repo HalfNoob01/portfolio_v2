1525c4aaac31a408de6c55c2caf3b869b6c62fa4
sanjay <mahatosanjay207@gmail.com>
2024-10-28 23:59:09 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,225 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Calender Scheduling Web App",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "You Tube Clone",
+    des: " YouTube clone with sleek, modern UI using React for dynamic content rendering and Material-UI (MUI) for stylish.",
+    img: "/youtube.svg",
+    iconLists: ["/re.svg", "/material.svg", "/js.svg", "/netlify.svg", "/redux.svg"],
+    link: "https://subtle-tanuki-4af470.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Nike - Static Website",
+    des: "Developed a visually appealing static website utilizing React for seamless UI components and Tailwind CSS for customizable, responsive design.",
+    img: "/nike.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/netlify.svg", "/redux.svg"],
+    link: "https://gregarious-moonbeam-19624a.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Facbook Clone",
+    des: "Created a Facebook clone using React for efficient front-end development and Firebase for real-time database management and authentication.",
+    img: "/fb.svg",
+    iconLists: ["/re.svg", "/material.svg", "/js.svg", "/firebase.svg", "/redux.svg"],
+    link: "https://facebook-12864.firebaseapp.com/",
+  },
+  {
+    id: 4,
+    title: "Travel Website",
+    des: "Created a travel website using Next.js and TypeScript, enhancing SEO and server-side rendering for faster page loads and improved user experience.",
+    img: "/travel.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/netlify.svg", "/re.svg"],
+    link: "https://jade-centaur-b2b8fc.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Sanjay was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Shahnawaz Alam",
+    title: "CEO & FOUNDER at SNMR CLOUD SERVICES",
+    img: '/shah.svg'
+  },
+  {
+    quote:
+      "Working with Sanjay was an enlightening experience. His innovative ideas and meticulous attention to detail were instrumental in our project's success. Sanjay not only met but exceeded our expectations with his solutions and proactive communication. For anyone seeking a transformative collaboration, Sanjay is the expert you need.",
+    name: "Sushant Shekhar",
+    title: "Full Stack Developer at SNMR CLOUD SERVICES",
+    img: '/sushant.svg'
+  },
+  {
+    quote:
+      "Partnering with Sanjay was remarkable from start to finish. His expertise and commitment brought a new level of sophistication to our endeavors. Sanjay's dedication to excellence and his punctuality in delivery truly distinguished him. If you need a specialist who can elevate your project, Sanjay is the go-to professional.",
+    name: "Mukesh Bara",
+    title: "Full Stack Developer at EtechCube",
+     img: '/mukesh.svg'
+  },
+  {
+    quote:
+      "Teaming up with Sanjay was a game-changer. His professional approach and dedication to achieving our project goals made him an invaluable partner. Sanjay's strategic insights and meticulous execution enabled us to surpass our expected outcomes. For results that truly stand out, choosing Sanjay is a wise decision.",
+    name: "Mohit Mahato",
+    title: "Full Stack Developer at EtechCube",
+     img: '/mohit.svg'
+  },
+  {
+    quote:
+      "Working with Sanjay has been a deeply rewarding experience. As his mentor, I've watched him grow immensely, absorbing knowledge and applying it with remarkable skill. His eagerness to learn and adapt is commendable, and his progress has been a testament to his dedication. Sanjay has become not just a proficient collaborator but also a true innovator in our field.",
+    name: "Samir",
+    title: "CCTV Operator at SNMR CLOUD SERVICES PRIVATE LIMITED",
+    img: "/samir.svg"
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern- EtechCube",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Web Developer - SNMR ",
+    desc: "Designed and developed Employee Management web app, platforms using React and Node.js.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/HalfNoob01"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Mahato_Sanjay01"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/sanjay-mahato-4aab59215/"
+  },
+];
